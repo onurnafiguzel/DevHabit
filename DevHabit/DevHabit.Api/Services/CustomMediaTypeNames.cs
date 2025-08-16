6327a3eef0080c8d4901de6bc5c4ca9dcd18e85b
Onur Güzel <ongguzel@gmail.com>
2025-08-17 01:31:50 +0300

    Adding the Users Resource

--- DevHabit/DevHabit.Api/Services/CustomMediaTypeNames.cs
+++ DevHabit/DevHabit.Api/Services/CustomMediaTypeNames.cs
@@ -9,5 +9,8 @@ public static class CustomMediaTypeNames
         public const string HateoasJson = "application/vnd.dev-habbit.hateoas+json";
         public const string HateoasJsonV1 = "application/vnd.dev-habbit.hateoas.1+json";
         public const string HateoasJsonV2 = "application/vnd.dev-habbit.hateoas.2+json";
+
+
+        public const string HateoasSubType = "hateoas";
     }
 }
