4ffdeff892958c513eb0a92353d934ab179d5a25
Onur Güzel <ongguzel@gmail.com>
2025-08-19 16:41:54 +0300

    Supporting Refresh Tokens

--- DevHabit/DevHabit.Api/Services/TokenProvider.cs
+++ DevHabit/DevHabit.Api/Services/TokenProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using DevHabit.Api.DTOs.Auth;
 using DevHabit.Api.Settings;
@@ -46,6 +47,8 @@ public sealed class TokenProvider(IOptions<JwtAuthOptions> options)
 
     private static string GenerateRefreshToken()
     {
-        return string.Empty;
+        byte[] randomBytes = RandomNumberGenerator.GetBytes(32);
+
+        return Convert.ToBase64String(randomBytes);
     }
 }
