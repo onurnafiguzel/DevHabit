287fa580e796ce8148c99160b34dcd4087512e33
Onur Güzel <ongguzel@gmail.com>
2025-08-22 00:16:29 +0300

    Encrytpion DI is added

--- DevHabit/DevHabit.Api/DependencyInjection.cs
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -161,6 +161,9 @@ public static class DependencyInjections
                 .Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
             });
 
+        builder.Services.Configure<EncryptionOptions>(builder.Configuration.GetSection("Encryption"));
+        builder.Services.AddTransient<EncryptionService>();
+
         return builder;
     }
 
