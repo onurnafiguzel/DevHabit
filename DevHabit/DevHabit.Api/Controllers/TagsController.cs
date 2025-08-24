b5c80785a6fa53b991b25ed2522f9002b657bfcc
Onur Güzel <ongguzel@gmail.com>
2025-08-25 00:32:33 +0300

    Optimistic Locking, Concurrency Control

--- DevHabit/DevHabit.Api/Controllers/TagsController.cs
+++ DevHabit/DevHabit.Api/Controllers/TagsController.cs
@@ -95,7 +95,7 @@ public sealed class TagsController(
     }
 
     [HttpPut("{id}")]
-    public async Task<ActionResult> UpdateTag(string id, UpdateTagDto updateTagDto)
+    public async Task<ActionResult> UpdateTag(string id, UpdateTagDto updateTagDto, InMemoryETagStore eTagStore)
     {
         string? userId = await userContext.GetUserIdAsync();
         if (string.IsNullOrWhiteSpace(userId))
@@ -113,6 +113,8 @@ public sealed class TagsController(
         tag.UpdateFromDto(updateTagDto);
 
         await dbContext.SaveChangesAsync();
+
+        eTagStore.SetETag(Request.Path.Value!, tag.ToDto());
 
         return NoContent();
     }
